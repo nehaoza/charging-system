2f41ff1d98f23c334f86fe9d4b8a51034a27edcd
Neha <nehaoza1307@gmail.com>
2023-04-11 12:13:23 +0100

    feat: fixing spotless issue

--- src/main/java/com/charging/system/domain/CommuterBus.java
+++ src/main/java/com/charging/system/domain/CommuterBus.java
@@ -8,7 +8,7 @@ public class CommuterBus extends ElectronicVehicle {
         super(chargePercentage);
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Set<Short> s = new HashSet<>();
         for (short i = 0; i < 100; i++) {
             s.add(i);
