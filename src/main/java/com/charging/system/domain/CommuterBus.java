ab8cafb77032490937a035bb27938d260046a74a
Neha <nehaoza1307@gmail.com>
2023-04-11 11:51:03 +0100

    feat: errorprone detection

--- src/main/java/com/charging/system/domain/CommuterBus.java
+++ src/main/java/com/charging/system/domain/CommuterBus.java
@@ -1,7 +1,19 @@
 package com.charging.system.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CommuterBus extends ElectronicVehicle {
     public CommuterBus(int chargePercentage) {
         super(chargePercentage);
+    }
+
+    public static void main (String[] args) {
+        Set<Short> s = new HashSet<>();
+        for (short i = 0; i < 100; i++) {
+            s.add(i);
+            s.remove(i - 1);
+        }
+        System.out.println(s.size());
     }
 }
